d0d4b56b0ca1ca356ca2acef176114fdcd3019d7
Banega00 <bane.stojanovic00@gmail.com>
2024-09-19 14:01:42 +0200

    compiled ts files to js

---
+++ src/appwrite-config.js
@@ -0,0 +1,7 @@
+"use strict";
+// export const AppwriteConfig = {
+//   APPWRITE_ENDPOINT:
+//     process.env.APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1',
+//   APPWRITE_PROJECT_ID: process.env.APPWRITE_PROJECT_ID,
+//   APPWRITE_API_KEY: process.env.APPWRITE_API_KEY,
+// };
